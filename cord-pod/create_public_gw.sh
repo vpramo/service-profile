3ab37c35cdae5b5d0d47d1b78e543de4618867c8
vpramo <vpramo@gmail.com>
2016-07-19 13:24:30 +0530

    Update create_public_gw.sh

--- cord-pod/create_public_gw.sh
+++ cord-pod/create_public_gw.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl --user karaf:karaf -X PUT -H "Content-Type: application/json" http://onos-ctrl1:8181/onos/v1/network/configuration/ -d @networkcfg.json
+curl --user karaf:karaf -X PUT -H "Content-Type: application/json" http://onos-xos-ctrl1:8181/onos/v1/network/configuration/ -d @networkcfg.json
