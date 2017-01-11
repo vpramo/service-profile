07d52ecfa54bac96c44573682a7d265f8e6f2106
Pramod Venkatesh <vpramo@Pramods-MacBook-Pro.local>
2017-01-11 07:42:01 +0530

    Added OPenrc for the setup

---
+++ cord-pod/admin-openrc.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Replace with the OpenStack admin credentials for your cluster
+export OS_TENANT_NAME=admin
+export OS_USERNAME=admin
+export OS_PASSWORD=Chang3M3
+export OS_AUTH_URL=http://openstack-xos-ctrl1:35358/v2.0
